1b3444d5d04f67ee07c6242d10473fffec8d27a8
Oğuzhan Aydınay <aydinayoguzhan@gmail.com>
2021-02-01 19:57:26 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,25 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car() { Id = 9, BrandId = 1, ColorId = 3,DailyPrice=3500, ModelYear=2020, Description="Car Description" };
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            foreach (var item in carManager.GetAll())
+            {
+                Console.WriteLine(item.Description);
+            }
+
+            carManager.Delete(car);
+            carManager.Add(car);
+            carManager.Update(car);
+            Console.WriteLine(carManager.GetById(3).Description);
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,59 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>() {
+                new Car{Id=1,BrandId=1,ColorId=1,DailyPrice=200,ModelYear=2013,Description="Ürün açıklaması 1"},
+                new Car{Id=2,BrandId=1,ColorId=2,DailyPrice=500,ModelYear=2013,Description="Ürün açıklaması 2"},
+                new Car{Id=3,BrandId=1,ColorId=5,DailyPrice=100,ModelYear=2013,Description="Ürün açıklaması 3"},
+                new Car{Id=4,BrandId=1,ColorId=7,DailyPrice=1000,ModelYear=2013,Description="Ürün açıklaması 4"},
+                new Car{Id=5,BrandId=1,ColorId=3,DailyPrice=350,ModelYear=2013,Description="Ürün açıklaması 5"},
+                new Car{Id=6,BrandId=1,ColorId=5,DailyPrice=344.99,ModelYear=2013,Description="Ürün açıklaması 6"},
+                new Car{Id=7,BrandId=1,ColorId=2,DailyPrice=657,ModelYear=2013,Description="Ürün açıklaması 7"}
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            var result = _cars.SingleOrDefault(a => a.Id == car.Id);
+            _cars.Remove(result);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public void Update(Car car)
+        {
+            var result = _cars.SingleOrDefault(a => a.Id == car.Id);
+            result.BrandId = car.BrandId;
+            result.ColorId = car.ColorId;
+            result.DailyPrice = car.DailyPrice;
+            result.Description = car.Description;
+            result.ModelYear = car.ModelYear;
+        }
+
+        Car ICarDal.GetById(int id)
+        {
+            var result = _cars.SingleOrDefault(a => a.Id == id);
+            return result;
+        }
+    }
+}
