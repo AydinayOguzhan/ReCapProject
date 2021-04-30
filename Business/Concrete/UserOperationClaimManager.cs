cc86739dd218c4b4e743ca83dd6095dca7df07df
Oğuzhan Aydınay <aydinayoguzhan@gmail.com>
2021-04-30 03:20:19 +0300

    Code refactor yapıldı

--- Business/Concrete/UserOperationClaimManager.cs
+++ Business/Concrete/UserOperationClaimManager.cs
@@ -39,31 +39,33 @@ namespace Business.Concrete
 
         public IResult CheckIfItsAdmin(int userId)
         {
-            int adminClaimId = 1;
-            var claims = _userOperationClaimDal.GetAll(u => u.UserId == userId);
-            if (claims == null)
+            var result = BusinessRules.Run(CheckAdminClaim(userId));
+            if (result == null)
             {
-                return new ErrorResult(Messages.UserNotFound);
+                int adminClaimId = 1;
+                var claims = _userOperationClaimDal.GetAll(u => u.UserId == userId);
+                var adminClaim = claims.SingleOrDefault(c => c.OperationClaimId == adminClaimId);
+                if (adminClaim == null)
+                {
+                    return new ErrorResult();
+                }
+                return new SuccessResult();
             }
-            var adminClaim = claims.SingleOrDefault(c => c.OperationClaimId == adminClaimId);
-            if (adminClaim == null)
-            {
-                return new ErrorResult();
-            }
-            return new SuccessResult();
+            return new ErrorResult(Messages.UserNotFound);
         }
 
         [SecuredOperations("admin")]
         [TransactionScopeAspect]
         public IResult Delete(UserOperationClaim userOperationClaim)
         {
-            int userClaim = 2;
-            if (userOperationClaim.OperationClaimId == userClaim)
+            var result = BusinessRules.Run(CheckIfDefaultOperation(userOperationClaim));
+            if (result == null)
             {
-                return new ErrorResult(Messages.ClaimCantDelete);
+                _userOperationClaimDal.Delete(userOperationClaim);
+                return new SuccessResult(Messages.Successful);
             }
-            _userOperationClaimDal.Delete(userOperationClaim);
-            return new SuccessResult(Messages.Successful);
+            return new ErrorResult(Messages.ClaimCantDelete);
+
         }
 
         public IDataResult<List<UserOperationClaim>> GetAll()
@@ -105,6 +107,26 @@ namespace Business.Concrete
         public IDataResult<List<UserOperationClaimDetailDto>> GetUserOperationClaimsByUserId(int userId)
         {
             return new SuccessDataResult<List<UserOperationClaimDetailDto>>(_userOperationClaimDal.GetUserOperationClaimDetailByUserId(userId));
+        }
+
+        private IResult CheckIfDefaultOperation(UserOperationClaim userOperationClaim)
+        {
+            int userClaim = 2;
+            if (userOperationClaim.OperationClaimId == userClaim)
+            {
+                return new ErrorResult();
+            }
+            return new SuccessResult();
+        }
+
+        private IResult CheckAdminClaim(int userId)
+        {
+            var claims = _userOperationClaimDal.GetAll(u => u.UserId == userId);
+            if (claims == null)
+            {
+                return new ErrorResult();
+            }
+            return new SuccessResult();
         }
     }
 }
